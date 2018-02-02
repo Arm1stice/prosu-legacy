a96eb34d91f007eca0227036dc351f83c4417fec
Wyatt Calandro <arm1stice@arm1stice.com>
2018-02-02 12:57:38 -0700

    Switch from coffee-script to coffee-register

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ require('./customFunctions');
 var processType = process.argv[2] || "web"
 
 if(processType === "web"){
-    require('coffee-script').register()
+    require('coffee-register')
     require("./web/index")
 }else if(processType === "worker"){
-    require('coffee-script').register()
+    require('coffee-register')
     require("./worker/index")
 }else{
     throw Error(`Invalid process type "${processType}"`)
